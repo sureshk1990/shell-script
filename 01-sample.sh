e5cce40224c3941518ec2c336d78fcdd5fa324af
Suresh <kkrlsuresh@gmail.com>
2021-08-31 09:57:54 +0000

    updated 2nd

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/bash
-ls 
-cat /etc/*release
+#ls 
+#cat /etc/*release
 
+# Any line start with '#' is treated as comment and it will ignore by the interpreter 
+# Printing a message in screen can be done by two commands
+# 1. echo
+# 2. printf
+# we choose echo command because of user frindlyness , yes it does all the jobs does by ptintf
+# syntax: echo message
+
+echo "hello World"
